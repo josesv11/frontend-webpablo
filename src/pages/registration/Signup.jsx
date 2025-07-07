dcc9ff2388bdc539e91948395bd6eaf3613fe2c0
josesv11 <143985741+josesv11@users.noreply.github.com>
2025-07-07 01:19:18 -0500

    Update Signup.jsx

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -38,7 +38,7 @@ const Signup = () => {
 
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/auth/register/", {
+      const response = awaitfetch(`${import.meta.env.VITE_API_URL}/api/auth/register/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
