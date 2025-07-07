3c798b5ce5bc849289b03f5c270173ed4bfb6306
josesv11 <143985741+josesv11@users.noreply.github.com>
2025-07-07 02:52:19 -0500

    Update ProductInfo.jsx

--- src/pages/productInfo/ProductInfo.jsx
+++ src/pages/productInfo/ProductInfo.jsx
@@ -25,7 +25,7 @@ const ProductInfo = () => {
   const getProductData = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`http://127.0.0.1:8000/api/products/${id}/`);
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/products/${id}/`);
 
       if (!response.ok) {
         throw new Error(`Error HTTP: ${response.status}`);
