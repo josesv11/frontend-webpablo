b84cc5a88d8032328459c7331dd936722c9cbbcf
josesv11 <143985741+josesv11@users.noreply.github.com>
2025-07-07 22:11:12 -0500

    Update AddProductPage.jsx

--- src/pages/admin/AddProductPage.jsx
+++ src/pages/admin/AddProductPage.jsx
@@ -41,7 +41,7 @@ const AddProductPage = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/products/", {
+       const res = await fetch(`${import.meta.env.VITE_API_URL}/api/products/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(product),
